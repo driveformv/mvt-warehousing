f0ad6470664d9528fc8354e620c54f5619f236ac
Hector Sanchez <hsanchez@driveformvt.com>
2025-04-10 23:38:19 -0600

    Fuck

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,6 +2,7 @@ import BlogPostClient from './blog-post-client';
 
 export const dynamic = 'force-dynamic';
 
-export default function BlogPostPage({ params }: { params: { id: string } }) {
-  return <BlogPostClient id={params.id} />;
+export default async function BlogPostPage({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params;
+  return <BlogPostClient id={resolvedParams.id} />;
 }
